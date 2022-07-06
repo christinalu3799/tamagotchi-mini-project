96ae066c503a8ea47d01a9b4d75c6a60ba5da4e7
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-07-06 08:48:45 -0400

    added instructions on how to play

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-let start = document.querySelector('#startGame');
-
+class Tamagotchi {
+    constructor(name, hunger=1, sleepiness=1, boredom=1, age=0) {
+        this.name = name;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom; 
+        this.age = age;
+    }
+}
 // Allow user to give their pet a name when they click button
 // Store in a variable called 'name'
 const giveName = () => {
@@ -11,25 +18,16 @@ const giveName = () => {
 
     // Create new element called 'greeting' to greet the user
     let greeting = document.createElement('h3')
-    greeting.innerHTML = `Congrats on your new pet! Interact with ${name} using the buttons below. Have fun!`
+    greeting.innerHTML = `Interact with ${name} using the buttons below. Keep ${name} ALIVE!`
 
     // Append this element to the dom
     let giveNameDiv = document.querySelector('#giveNameDiv')
     giveNameDiv.appendChild(greeting)
+
+    // Instantiate a new Tamagotchi pet from the Tamagotchi class
+    let myTamagotchi = new Tamagotchi(name)
+    console.log(myTamagotchi)
     return name
 }
-
+// Listen for button click for when user wants to name their pet
 let name = document.querySelector('#name').addEventListener('click',giveName)
-
-class Tamagotchi {
-    constructor(name, hunger, sleepiness, boredom, age=0) {
-        this.name = name;
-        this.hunger = hunger;
-        this.sleepiness = sleepiness;
-        this.boredom = boredom; 
-        this.age = age;
-    }
-}
-
-// let myTamagotchi = new Tamagotchi(giveName())
-// console.log(myTamagotchi)
